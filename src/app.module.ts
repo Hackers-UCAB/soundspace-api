94503b4e6956064ebdfa61f84e646efc033745c9
Victor Blanco <victorblanco281201@gmail.com>
2023-12-10 11:25:27 -0400

    Actualizando indicaciones para clonar

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,6 @@ import { databaseProviders } from 'dbconfig';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DB_HOST,
-    //   port: +process.env.DB_PORT,
-    //   database: process.env.DB_NAME,
-    //   username: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
-
   ],
   controllers: [AuthController],
   providers: [...databaseProviders],
