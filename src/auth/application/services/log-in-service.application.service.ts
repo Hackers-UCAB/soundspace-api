2629c35654abac5dbf956697c85c5686427bfed2
Guiniel <gitllermo23@gmail.com>
2023-12-11 19:34:24 -0400

    algo

--- src/auth/application/services/log-in-service.application.service.ts
+++ src/auth/application/services/log-in-service.application.service.ts
@@ -1,9 +1,7 @@
 import { IApplicationService } from 'src/common/application/services/interfaces/application-service.interface';
 import { IUserRepository } from 'src/user/domain/repositories/user.repository.interface';
 import { Result } from 'src/common/application/result-handler/result';
-import { UserPhone } from 'src/user/domain/value-objects/user-phone';
 import { AuthApplicationDto } from '../dto/auth.application.dto';
-import { OrmUserEntity } from 'src/user/infraestructure/orm-entities/user.entity';
 import { User } from 'src/user/domain/user';
 
 
--- src/common/application/services/decorators/auditing-decorator/auditing-application-service.decorator.ts
+++ src/common/application/services/decorators/auditing-decorator/auditing-application-service.decorator.ts
@@ -1,12 +1,31 @@
-import { IApplicationService } from "../../interfaces/application-service.interface";
-import { ApplicationServiceDecorator } from "../application-service.decorator";
+// import { IAuditingRepository } from "src/common/application/repositories/auditing.repository";
+// import { IApplicationService } from "../../interfaces/application-service.interface";
+// import { ApplicationServiceDecorator } from "../application-service.decorator";
+// import { Result } from "src/common/application/result-handler/result";
 
 
-export class AuditingCommandServiceDecorator<D,R> extends ApplicationServiceDecorator<D,R> {
-    private readonly auditing
+// export class AuditingCommandServiceDecorator<D,R> extends ApplicationServiceDecorator<D,R> {
+//     private readonly auditingRepository: IAuditingRepository;
 
-    constructor(applicationService: IApplicationService<D, R>) {
-        super(applicationService);
+//     constructor(applicationService: IApplicationService<D, R>, auditingRepository: IAuditingRepository) {
+//         super(applicationService);
+//         this.auditingRepository = auditingRepository;
+//     }
 
-    }
-}
+//     execute(param: D): Promise<Result<R>> {
+//         const r = super.execute(param);
+//         this.Auditing(param);
+//         return r
+//     }
+
+//     private Auditing(param: D): void {
+//         const entry: AuditEntry = {
+//             userId: this.userContext.currentUser.id,
+//             timeOfExecution: this.timeProvider.now,
+//             operation: command.constructor.name,
+//             data: JSON.stringify(command)
+//         };
+
+//         this.auditingRepository.save(entry);
+//     }
+// }
