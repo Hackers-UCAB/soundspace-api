f7ce253ed20b2d23af93c876dc3fa27fab75f7eb
Jhonnysour <jhonnysour26@gmail.com>
2023-12-29 19:10:00 -0400

    Implementación del metodo findAlbumsByArtist

--- src/album/infraestructure/repositories/album.repository.ts
+++ src/album/infraestructure/repositories/album.repository.ts
@@ -2,9 +2,11 @@ import { DataSource, Repository } from 'typeorm';
 import { Result } from 'src/common/application/result-handler/result';
 import { IAlbumRepository } from 'src/album/domain/repositories/album.repository.interface';
 import { Album } from '../../domain/album';
+import { Artist } from 'src/artist/domain/artist';
 import { OrmPlaylistEntity } from '../../../common/infraestructure/orm-entities/playlist.entity';
 import { OrmAlbumMapper } from '../mapper/orm-album.mapper';
 import { AlbumId } from '../../domain/value-objects/album-id';
+import { ArtistId } from 'src/artist/domain/value-objects/artist-id';
 export class AlbumRepository
   extends Repository<OrmPlaylistEntity>
   implements IAlbumRepository
@@ -14,15 +16,60 @@ export class AlbumRepository
     super(OrmPlaylistEntity, dataSource.createEntityManager());
     this.OrmAlbumMapper = new OrmAlbumMapper();
   }
+
+  async findAlbumsByArtist(artistId: ArtistId): Promise<Result<Album[]>> {
+    let response: Album[];
+    let error: Error;
+    try {
+      const albums = await this.createQueryBuilder('playlist')
+        .select([
+          'playlist.codigo_playlist',
+          'playlist.nombre',
+          'playlist.referencia_imagen',
+          'cancion.codigo_cancion',
+        ])
+        .innerJoinAndSelect('playlist.canciones', 'playlistCancion')
+        .innerJoinAndSelect('playlistCancion.cancion', 'cancion')
+        .innerJoin(
+          'cancion_artista',
+          'ca',
+          'ca.codigo_cancion = cancion.codigo_cancion',
+        )
+        .innerJoinAndSelect('ca.artista', 'artista')
+        .where('artista.codigo_artista = :artistId', { artistId: artistId.Id })
+        .where("playlist.tipo = 'album'")
+        .getMany();
+
+      console.log('albums repo', albums);
+
+      response = await Promise.all(
+        albums.map(async (album) => await this.OrmAlbumMapper.toDomain(album)),
+      );
+    } catch (e) {
+      error = e;
+    } finally {
+      if (error) {
+        return Result.fail(
+          null,
+          500,
+          error.message ||
+            'Ha ocurrido un error obteniendo la lista de álbumes por artista, hable con el administrador',
+          error,
+        );
+      }
+      return Result.success<Album[]>(response, 200);
+    }
+  }
+
   async findAlbumById(albumId: AlbumId): Promise<Result<Album>> {
     let response: Album;
     let error: Error;
-      try {
-          console.log("aja repo");
-          console.log("aja repo");
-          console.log("aja repo");
-          console.log("aja repo");
-          console.log("aja repo");
+    try {
+      console.log('aja repo');
+      console.log('aja repo');
+      console.log('aja repo');
+      console.log('aja repo');
+      console.log('aja repo');
 
       const album = await this.createQueryBuilder('playlist')
         .select([
@@ -35,7 +82,7 @@ export class AlbumRepository
         .innerJoinAndSelect('playlistCancion.cancion', 'cancion')
         .where("playlist.tipo = 'album'")
         .where('playlist.codigo_playlist = :id', { id: albumId.Id })
-              .getOne();
+        .getOne();
 
       response = await this.OrmAlbumMapper.toDomain(album);
     } catch (e) {
@@ -58,7 +105,7 @@ export class AlbumRepository
     let response: Album[];
     let error: Error;
     try {
-      console.log("aja repo");
+      console.log('aja repo');
       const albums = await this.createQueryBuilder('playlist')
         .select([
           'playlist.codigo_playlist',
@@ -71,8 +118,8 @@ export class AlbumRepository
         .where('playlist.trending = :trending', { trending: true })
         .where("playlist.tipo = 'album'")
         .getMany();
-        
-        console.log("albums repo",albums);
+
+      console.log('albums repo', albums);
       response = await Promise.all(
         albums.map(
           async (albums) => await this.OrmAlbumMapper.toDomain(albums),
@@ -86,7 +133,7 @@ export class AlbumRepository
           null,
           500,
           error.message ||
-            'Ha ocurrido un error inesperado obteniendo el album, hable con el administrador',
+            'Ha ocurrido un error inesperado obteniendo la lista de albums top, hable con el administrador',
           error,
         );
       }
