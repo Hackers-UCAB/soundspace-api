4295942a89cf2afcacd3d1ea0c5e392827fc8f47
Guiniel <gitllermo23@gmail.com>
2024-01-12 23:08:39 -0400

    Commit para cambiarme de rama

---
+++ test/common/objects-mother/promotion.object-mother.ts
@@ -0,0 +1,21 @@
+import { UuidGenerator } from "src/common/infraestructure/uuid-generator";
+import { Promotion } from "src/promotions/domain/promotion";
+import { PromotionId } from "src/promotions/domain/value-objects/promotion-id";
+import { PromotionUrl } from "src/promotions/domain/value-objects/promotion-url";
+
+
+// export class PromotionObjectMother{
+
+//     static createNormalUser(){
+//         const idGenerator = new UuidGenerator()
+    
+//         const randomPromotion = Promotion.create(
+//             PromotionId.create(idGenerator.generate()),
+//             PromotionUrl.create('https://promocion.com'),
+//         )
+        
+//         return normalUser;
+//     }
+
+// }
+
---
+++ test/promotion/get-random-promotion.service.spec.ts
@@ -0,0 +1,18 @@
+
+describe('Buscar una promocion aleatoria', () => {
+    it('Devuelve una promocion', async () => {
+        //arrange
+
+
+
+        //act
+
+
+
+        //assert
+
+
+    })
+
+
+})
