cf96e05d2fd1883368f9272546165b5540247bcc
Guiniel <gitllermo23@gmail.com>
2024-01-14 13:05:24 -0400

    Version nueva de stream de una cancion

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Range',
   });
 
   await app.listen(process.env.PORT);
