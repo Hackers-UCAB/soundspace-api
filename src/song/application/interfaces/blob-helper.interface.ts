506742eeca7ac8219ee7f0e2612d08bf4a557671
Guiniel <gitllermo23@gmail.com>
2024-01-15 17:00:30 -0400

    Se limpia song, se acomodan los nombres de archivo para el stream y ya te deberia traer la cancion de los pollitos

--- src/song/application/interfaces/blob-helper.interface.ts
+++ src/song/application/interfaces/blob-helper.interface.ts
@@ -1,5 +1,4 @@
 
-export interface IBlobHelper {
-    getBlobClient(fileName: string, container: string): Promise<any>
-    getFile(fileName: string, container: string, startPointInSeconds: number, duration: number): Promise<any>
+export interface IGetSongHelper {
+    getFile(fileName: string, container: string, startPointInSeconds: number): Promise<any>
 }
